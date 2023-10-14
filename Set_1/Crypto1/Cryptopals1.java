7e517d7c81aa4a976369f7cbb0b06af40b3ab640
daviludvig <ludviglongen@gmail.com>
2023-10-14 18:16:33 -0300

    Adicionei um comentario

--- Set_1/Crypto1/Cryptopals1.java
+++ Set_1/Crypto1/Cryptopals1.java
@@ -4,7 +4,7 @@ import Utils.ByteArray;
 import Utils.Hex_ByteArray;
 
 public class Cryptopals1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Isso e um comentario
         String hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         byte[] array = Hex_ByteArray.convert(hexString);
         String b64String = ByteArray.byteB64String(array);
